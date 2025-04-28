60d763ce30b7ebbaf72db906034becd90d639317
surbhi <surbhibisarwal79@gmail.com>
2025-04-28 15:54:42 +0530

    E-Commerce

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
+import { ProductsListComponent } from './pages/products-list/products-list.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  imports: [HeaderComponent, RouterOutlet],
+  template: `<app-header /> <router-outlet></router-outlet>`,
+  styles: ``,
 })
-export class AppComponent {
-  title = 'my-project';
-}
+export class AppComponent {}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,22 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { signal, Component, inject } from '@angular/core';
+import { PrimaryButtonComponent } from '../primary-button/primary-button.component';
+import { CartService } from '../../services/cart.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
-  template: `<p>header works!</p>
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>`,
-  styleUrl: './header.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [PrimaryButtonComponent, RouterLink],
+  template: `<div
+    class="bg-slate-100 px-4 py-3 shadow-md flex justify-between items-center"
+  >
+    <button class="text-xl" routerLink="/">My Store</button>
+    <app-primary-button
+      [label]="'Cart(' + cartService.cart().length + ')'"
+      routerLink="/cart"
+    />
+  </div>`,
+  styles: ``,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  cartService = inject(CartService);
+}
